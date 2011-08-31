9c2f2b8ca8efe4c07e8b77457210fbc604bc49ef
hol353 <hol353@31490d77-854b-a441-874d-f8801a3780c9>
2011-08-31 04:30:03 +0000

    Changes to ProductRegistration to use DataTableToCSV function in CSGeneral.
    git-svn-id: file://fsqld3-tw/SE-Home/hol353/SVNRepository/ProductRegistrationWeb@170 31490d77-854b-a441-874d-f8801a3780c9

--- ViewRegistrations.aspx.cs
+++ ViewRegistrations.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
+using CSGeneral;
 
 namespace ProductRegistration
 {
@@ -20,36 +21,9 @@ namespace ProductRegistration
 
             System.Data.DataView dv = (DataView) SqlDataSource1.Select(new DataSourceSelectArguments());
             DataTable Data = dv.ToTable();
-            string St = "";
-            for (int i = 1; i < Data.Columns.Count; i++)
-            {
-                if (i > 1)
-                    St += ", ";
-                St += Data.Columns[i].ColumnName;
-            }
-            Response.Write(St + "\r\n");
-
-            foreach (DataRow Row in Data.Rows)
-            {
-                St = "";
-                for (int i = 1; i < Data.Columns.Count; i++)
-                {
-                    if (i == 1)
-                    {
-                        DateTime D = Convert.ToDateTime(Row[i]);
-                        St = D.ToShortDateString();
-                    }
-                    else
-                    {
-                        St += ", " + Row[i].ToString();
-                    }
-                }
-                
-                Response.Write(St + "\r\n");
-            }
+            Response.Write(DataTableUtility.DataTableToCSV(Data, 1));
             Response.Flush();                 // send our content to the client browser.
             Response.SuppressContent = true;  // stops .net from writing it's stuff.
-
         }
     }
 }
